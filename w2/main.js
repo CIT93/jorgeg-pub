c9f5f331196c0b666fff23274a321642cf4e9197
gurru1986 <jgurrusquieta1@live.com>
2024-01-18 23:50:29 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,11 @@
+const houseHold = 4;
+const apartment = 2;
+const meatBalance = 10+6;
+const midWasher = 4;
+const needyFam = 8;
+const cleanHouse = 40;
+const semiRecycler = 24 - 12
+const uberPlus = 12
+// don't take transportation or flights.
+// Only persoanl vehicle usage was added
+const carbonTotal = houseHold + apartment + meatBalance + midWasher + needyFam + cleanHouse + semiRecycler + uberPlus

63e314c3e7336e57d4a0416eb1b4c732118a632c
gurru1986 <jgurrusquieta1@live.com>
2024-01-30 22:52:10 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -7,7 +7,7 @@
 // If you share a house or apartment with more than 5 other people, then add 2 points.
 
 //setting value in function() is considered parameters. note! arguments get passed into parameters
-function determineHouseHoldPts() {
+function determineHouseHoldPts(numberInHousehold) {
   console.log("Insided the Function");
   if (numberInHousehold === 0) {
     carbonFootprintPoints += 14;
@@ -30,7 +30,58 @@ function determineHouseHoldPts() {
 }
 
 let carbonFootprintPoints = 0;
-const numberInHousehold = 3;
+//const numberInHousehold = 3;
 //setting value in (), makes it into an argument
 determineHouseHoldPts(3);
 determineHouseHoldPts(4);
+//=============================================================================================
+
+let wrongAnswer = "";
+function pointsforHouseSize (houseSize){
+    switch(houseSize){
+        case "large":
+            houseSize = 10;
+            houseInfo.textContent = `Carbon Footprint is ${houseSize} pts.`;
+            break;
+        case "medium":
+            houseSize = 7;
+            houseInfo.textContent = `Your carbon Footprint is ${houseSize} pts.`;
+            break;
+        case "small":
+            houseSize = 4;
+            houseInfo.textContent = `Your carbon Footprint is ${houseSize} pts.`;
+        case "apartment":
+            houseSize = 2;
+            houseInfo.textContent = `Your Carbon Footprint is ${houseSize}pts.`;
+            break;
+        default:
+            wrongAnswer = window.prompt("Sorry no nintendo!");
+            console.log(houseSize)
+            break;
+    }
+}
+
+
+let yourName = window.prompt("What is your name?");
+//.getElementById gets  div greeting in HTML, its required to display in document
+let greetingElement = document.getElementById("greeting");
+
+//.textContent allows to display text on documnent.
+greetingElement.textContent = `Hey ${yourName}`;
+
+let houseSize = window.prompt(
+  "And what is your house size? Choose a number. Large is 10, medium is 7, small is 4, apartment is 2."
+);
+let houseInfo = document.getElementById("house");
+
+//convert into an Integer
+//houseSize = parseInt(houseSize);
+
+pointsforHouseSize(houseSize)
+
+/* rules for setting up text from javaScript to HTML document
+1. setup link with "document.getElementById(id selector or class from html)"
+2. use variable.textContent = "and whatever you wanna ouput" */
+
+
+

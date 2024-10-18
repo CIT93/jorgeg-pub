62ad3eb3f86a521cfbe6e3624a5f0feb9e44f58c
Jorge5G <jorge.garcia.4x7@gmail.com>
2024-10-17 18:31:23 -0700

    w10 my attempt at the code, before I review other students code

--- w10/main.js
+++ w10/main.js
@@ -3,7 +3,7 @@ import {determineHouseHoldPts, determineHouseSizePts} from "./carbonFootprint.js
 import {FORM, FNAME, LNAME, SUBMIT} from "./global.js"
 import {saveLS, cfpData} from "./storage.js"
 
-const start = function(first, last, houseHoldMembers, houseSize,) {
+const start = (first, last, houseHoldMembers, houseSize,) => {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePTS = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePTS;
@@ -21,7 +21,7 @@ const start = function(first, last, houseHoldMembers, houseSize,) {
   renderTbl(cfpData);
 
   // Function to validate a single field
-  const validateField = function(event) {
+  const validateField = event => {
     const field = event.target.value;
     const fieldId = event.target.id;
     const fieldError = document.getElementById(`${fieldId}Error`);
@@ -40,7 +40,7 @@ const start = function(first, last, houseHoldMembers, houseSize,) {
   LNAME.addEventListener('blur', validateField);
 
 
-  FORM.addEventListener('submit', function(e){
+  FORM.addEventListener('submit', e => {
     e.preventDefault();
     if (FNAME.value !== '' && LNAME.value !== '') {
       SUBMIT.textContent = '';
@@ -61,11 +61,11 @@ const start = function(first, last, houseHoldMembers, houseSize,) {
 // const result = add2(1, 2, 3, 4);
 
 // arrow function
-const add2 = function(...a) {
-  return 2 + a[3];
-}
 
-const result = add2(1, 2, 3, 4);
+const add2 = a => 2 + a;
+
+
+const result = add2(100);
 
 // (a = 10) default value
 
--- w10/render.js
+++ w10/render.js
@@ -1,13 +1,13 @@
 import {FORM, TBL} from "./global.js"
 import { saveLS } from "./storage.js";
 
-const renderTblHeading = function() {
+const renderTblHeading = (...headings) => {
   //TBL.innerHTML = "";
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tr = document.createElement("tr");
-    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
-    headingTextArr.forEach(function(text){
+   
+    headings.forEach(text => {
       const th = document.createElement("th");
       th.textContent = text;
       tr.appendChild(th);
@@ -17,13 +17,29 @@ const renderTblHeading = function() {
     return table;
   }
 
-  const onUpdate = function(index, data) {
+  // const renderTblHeading = () => {
+  //   //TBL.innerHTML = "";
+  //     const table = document.createElement("table");
+  //     const thead = document.createElement("thead");
+  //     const tr = document.createElement("tr");
+  //     const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+  //     headingTextArr.forEach(text => {
+  //       const th = document.createElement("th");
+  //       th.textContent = text;
+  //       tr.appendChild(th);
+  //     });
+  //     thead.appendChild(tr);
+  //     table.appendChild(thead);
+  //     return table;
+  //   }
+
+  const onUpdate = (index, data) => {
     data.splice(index, 1);
     saveLS(data);
     renderTbl(data);
   }
 
-  const renderTblBtn = function(obj, index, data){
+  const renderTblBtn = (obj, index, data) => {
     const td = document.createElement("td");
     const btnEdit = document.createElement("button");
     const btnDel = document.createElement("button");
@@ -31,10 +47,10 @@ const renderTblHeading = function() {
     btnDel.textContent = "Del";
     td.appendChild(btnEdit);
     td.appendChild(btnDel);
-    btnDel.addEventListener('click', function(e){
+    btnDel.addEventListener('click', e => {
       onUpdate(index, data);
     })
-    btnEdit.addEventListener('click', function(e){
+    btnEdit.addEventListener('click', e => {
       FORM[1].value = obj.firstName;
       FORM[2].value = obj.lastName;
       FORM[3].value = obj.houseM;
@@ -44,9 +60,9 @@ const renderTblHeading = function() {
     return td;
   }
 
-  const renderTblBody = function(data){
+  const renderTblBody = data => {
     const tbody = document.createElement("tbody");
-    data.forEach(function (obj, index) {
+    data.forEach((obj, index) => {
       const tr = document.createElement("tr");
       for (const [key, value] of Object.entries(obj)) {
         if (key !== "lastName" && key !== "houseMPTS" && key !== "houseSPTS"){
@@ -62,10 +78,10 @@ const renderTblHeading = function() {
     return tbody;
   }
 
-  const renderTbl = function(data){
+  const renderTbl = data => {
     TBL.innerHTML = "";
     if (data.length !== 0) {
-      const table = renderTblHeading();
+      const table = renderTblHeading("Name", "Household", "HouseSize", "Footprint", "Actions");
       const tbody = renderTblBody(data);
       table.appendChild(tbody);
       TBL.appendChild(table);

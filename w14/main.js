65b7606b15840e35d2ff10ba6532f0e1b5b450b9
Jorge5G <jorge.garcia.4x7@gmail.com>
2024-11-14 16:24:43 -0800

    w14 my attempt at the code, before I review other students or solution code

--- w14/main.js
+++ w14/main.js
@@ -1,2 +1,33 @@
+async function getPhotos() {
+  const url = "https://jsonplaceholder.typicode.com/photos";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
+    if (response.status === 200) {
+      console.log("Photos successfully fetched.");
+      renderPhotos(data);
+    } else {
+      console.log("Server Error");
+    }
+  } catch (error) {
+    console.log("Fetch Error", error);
+  }
+}
+
+function renderPhotos(photos) {
+  const OUTPUT = document.getElementById("output");
+
+  photos.forEach((img) => {
+    const imgEl = document.createElement("img");
+    imgEl.setAttribute("src", img.thumbnailUrl);
+    OUTPUT.appendChild(imgEl);
+  });
+}
+
+async function start() {
+  await getPhotos();
+}
+
+start();
 
